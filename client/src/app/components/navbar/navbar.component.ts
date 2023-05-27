9a1c93fd883efc32ec74385404e49670a62924a8
Pavel <M3modelist@gmail.com>
2023-05-27 21:06:32 +0300

    add navbar dropdown

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from "@angular/core";
 import {AuthService} from "../../@core/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent {
-  constructor(protected auth: AuthService) {
+
+  isDropdownOpen = false;
+
+  constructor(protected auth: AuthService, private router: Router) {
+  }
+
+  @HostListener("click", ["$event.target"]) onClick(event: Event) {
+    console.log(event);
+  };
+
+  showDropdownMenu(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  navigateTo() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 }
